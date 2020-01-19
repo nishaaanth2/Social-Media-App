e38a0af4f048d69f36a16892249600de00285c2b
nyow <itismenishaanth@gmail.com>
2020-01-19 21:53:26 +0530

    First My Msg

---
+++ app/src/main/java/com/example/soc/Activity/RegisterActivty.java
@@ -0,0 +1,19 @@
+package com.example.soc.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.soc.R;
+
+public class RegisterActivty extends AppCompatActivity {
+private Button b;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+}
